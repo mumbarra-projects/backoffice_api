fc570580141a79b226fe8f8226236ab79e627940
lucasmumbarra <lucassantosm.2014@gmail.com>
2024-01-04 15:54:53 -0300

    Update user

--- src/modules/user/useCases/update.user.ts
+++ src/modules/user/useCases/update.user.ts
@@ -1,8 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UserRequest } from '../dtos/user.request';
 import { UserResponse } from '../dtos/user.response';
 import { UserRepository } from '../user.repository';
 import { UserMapping } from '../user.mapping';
+import { UserModel } from '../dtos/user.model';
 
 @Injectable()
 export class UpdateUser {
@@ -19,9 +20,37 @@ export class UpdateUser {
       throw new NotFoundException('User not found');
     }
 
+    await this.validateExist(request, model);
+
     const data = this.mapping.update(model, request);
 
     const result = await this.repository.update(data, uuid);
     return this.mapping.response(result)
+  }
+
+  private async validateExist(request: UserRequest, model: UserModel) {
+    if (request.username !== model.username) {
+      const existUsername = await this.repository.existByUsername(request.username);
+
+      if (existUsername) {
+        throw new BadRequestException('User with this username already exists');
+      }
+    }
+
+    if (request.document !== model.document) {
+      const existByDocument = await this.repository.existByDocument(request.document);
+
+      if (existByDocument) {
+        throw new BadRequestException('User with this document already exists');
+      }
+    }
+
+    if (request.email && request.email !== model.email) {
+      const existByEmail = await this.repository.existByEmail(request.email);
+
+      if (existByEmail) {
+        throw new BadRequestException('User with this email already exists');
+      }
+    }
   }
 }
